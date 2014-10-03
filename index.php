41b6fd3b2c5b74239b0b90a47763b4cdea1ce83d
Ashley Daniell <adaniell@hbs.edu>
2014-10-03 16:07:09 -0400

    validating html

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     		<p>How many words? <input maxlength='1' type='text' name='length' size='1'/> (Max 8)</p>
     		<p>Include numbers? Yes <input type='checkbox' name='number'/></p>
    		    <p>Include symbols? <span class="small">(i.e. !@#)</span> Yes <input type='checkbox' name='symbol'/></p>
-		    <input type='submit' value='Generate My Password!' class='btn'/><br>
+		    <input type='submit' value='Generate My Password!' class='btn'/><br/>
 		</form>
 
 		<div class="password">
